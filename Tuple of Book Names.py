dc7117b11a581b75f2b64be5278a0f281ae8b53d
Dayve_David <willohblack@gmail.com>
2025-07-31 15:24:22 +0200

    Create Tuple of Book Names.py

---
+++ Tuple of Book Names.py
@@ -0,0 +1,7 @@
+# Tuple of favorite books
+books = ("1984", "The Hobbit", "To Kill a Mockingbird", "Dune", "The Catcher in the Rye")
+
+# Print each book on a separate line using a for loop
+print("My favorite books:")
+for book in books:
+    print(book)
