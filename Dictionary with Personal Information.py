054abccb1e8b4cd7a2fba20f4a5cd7f7d5332ecb
Dayve_David <willohblack@gmail.com>
2025-07-31 15:24:45 +0200

    Create Dictionary with Personal Information.py

---
+++ Dictionary with Personal Information.py
@@ -0,0 +1,11 @@
+# Create an empty dictionary
+person = {}
+
+# Ask user for input
+person["name"] = input("Enter your name: ")
+person["age"] = input("Enter your age: ")
+person["favorite_color"] = input("Enter your favorite color: ")
+
+# Print the dictionary
+print("\nPerson Information:")
+print(person)
